1510ba32afb98d62a5861b1c60e544038c4005a9
vonstruddle <quentin.durantay@gmail.com>
2016-09-23 06:56:03 +0000

    Add simple Hello World-like response

--- HelloReact/server.js
+++ HelloReact/server.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/', function(req, res) {
+  res.send('Hello Quentin!');
+});
+
 app.listen(8080, '0.0.0.0', function() {
-  console.log('Server up on port 8080')
+  console.log('Server up on port 8080');
 });
