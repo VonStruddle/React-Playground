9d8abf6bccbe42dc8cf329f497da8e2b5db04269
vonstruddle <quentin.durantay@gmail.com>
2016-09-23 18:47:37 +0000

    Move to ES6 inside of JSX and use nested component

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,24 +1,35 @@
-var Greeter = React.createClass({
-	getDefaultProps: function() {
+let GreeterMessage = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>This is from React.</p>
+      </div>
+    );
+  }
+});
+
+let Greeter = React.createClass({
+	getDefaultProps() {
 	    return {
 	    	name: 'You',
 	    	message: 'Welcome to React.'
 	    };
 	},
 
-  getInitialState: function() {
+  getInitialState() {
     return {
       name: this.props.name
     };
   },
 
-  onButtonClick: function(e) {
+  onButtonClick(e) {
     e.preventDefault();
-    
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
+
+    let nameRef = this.refs.name;
+    let name = nameRef.value;
     nameRef.value = '';
-    
+
     if (typeof name === 'string' && name.length > 0) {
       this.setState({
         name: name
@@ -26,14 +37,16 @@ var Greeter = React.createClass({
     }
   },
 
-	render: function() {
-    var name = this.state.name;
-    var message = this.props.message;
+	render() {
+    let name = this.state.name;
+    let message = this.props.message;
 
 		return (
 			<div>
 				<h1>Hello {name}!</h1>
 				<p>{message}</p>
+
+        <GreeterMessage/>
 
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
@@ -44,8 +57,8 @@ var Greeter = React.createClass({
 	}
 });
 
-var name = 'Quentin';
-var message = 'Welcome to React, the best JS library.';
+let name = 'Quentin';
+let message = 'Welcome to React, the best JS library.';
 
 ReactDOM.render(
   <Greeter name={name} message={message}/>,
