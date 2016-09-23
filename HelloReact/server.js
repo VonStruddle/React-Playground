5ccf780729e57b6c36a51ae26e4545c534b418a1
vonstruddle <quentin.durantay@gmail.com>
2016-09-23 08:52:25 +0000

    Add index page to app

--- HelloReact/server.js
+++ HelloReact/server.js
@@ -4,9 +4,9 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
   res.send('Hello Quentin!');
-});
+});*/
 
 app.listen(8080, '0.0.0.0', function() {
   console.log('Server listening on port 8080');
