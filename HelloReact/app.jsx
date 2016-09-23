e50c5970aafcc35b6e2f2df5824e4ce0becd58e0
Quentin Durantay <quentin.durantay@gmail.com>
2016-09-23 19:14:25 +0200

    Use of component properties

--- HelloReact/app.jsx
+++ HelloReact/app.jsx
@@ -1,15 +1,25 @@
 var Greeter = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	    	name: 'You',
+	    	message: 'Welcome to React.'      
+	    };
+	},
+
 	render: function() {
 		return (
 			<div>
-				<h1>Hello from React!</h1>
-				<p>My first JS framework :D</p>
+				<h1>Hello {name}!</h1>
+				<p>{message}</p>
 			</div>
 		);
 	}
 });
 
+var name = 'Quentin';
+var message = 'Welcome to React, the best JS library.';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={name} message={message}/>,
   document.getElementById('app')
 );
