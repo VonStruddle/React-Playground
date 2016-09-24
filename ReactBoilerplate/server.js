92c7ce6107541016b9cb1d2ffacf2be3717ac1b1
vonstruddle <quentin.durantay@gmail.com>
2016-09-24 09:38:47 +0000

    Add Boilerplate app to project

---
+++ ReactBoilerplate/server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+/*app.get('/', function(req, res) {
+  res.send('Hello Quentin!');
+});*/
+
+const PORT = 3000
+
+app.listen(PORT, '0.0.0.0', function() {
+  console.log('Server listening on port ' + PORT);
+});
