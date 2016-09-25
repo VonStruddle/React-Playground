cc2277259e87a0c133e2b9723804ca66910ab41c
vonstruddle <quentin.durantay@gmail.com>
2016-09-25 22:01:45 +0000

    Hotfix for Heroku and ExpressJS

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -4,11 +4,10 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  if (req.protocol === 'http') {
-    next();
-  } else {
+  if (req.protocol === 'https') {
     res.redirect('http://' + req.hostname + req.url);
-  }
+  } else {
+    next();
 });
 
 app.use(express.static('public'));
