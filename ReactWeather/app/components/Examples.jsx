33a6119162c44e61a82c4834370dc266d25bff89
vonstruddle <quentin.durantay@gmail.com>
2016-09-26 20:35:27 +0000

    Add styles to pages and examples to try

--- ReactWeather/app/components/Examples.jsx
+++ ReactWeather/app/components/Examples.jsx
@@ -1,11 +1,20 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 let Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page.</p>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are some examples you can try:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Bordeaux'>Bordeaux, France</Link>
+        </li>
+        <li>
+          <Link to='/?location=New%20York'>New York, USA</Link>
+        </li>
+      </ol>
     </div>
-  ); 
+  );
 }
 module.exports = Examples;
