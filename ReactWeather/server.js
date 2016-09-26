ad0abf6917967e1a7d7f02f886353861515d6f3c
vonstruddle <quentin.durantay@gmail.com>
2016-09-26 08:54:42 +0000

    Prep for Heroku according to Andrew

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+var express = require('express');
 
-let app = express();
+var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT, '0.0.0.0', function() {
-  console.log('Server listening on port ' + PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
