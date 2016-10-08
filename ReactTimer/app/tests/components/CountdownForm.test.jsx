e0ef0a4f485584fbe43804c5bc3e4e0c33bc8bc9
VonStruddle <quentin.durantay@gmail.com>
2016-10-08 11:01:29 +0100

    Add more advanced tests for CountdownForm comp

--- ReactTimer/app/tests/components/CountdownForm.test.jsx
+++ ReactTimer/app/tests/components/CountdownForm.test.jsx
@@ -10,4 +10,26 @@ describe('CountdownForm', () => {
   it('should exist', () => {
     expect(CountdownForm).toExist();
   });
+
+  it('should call onSetCountdown if valid seconds entered', () => {
+    var spy = expect.createSpy();
+    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = '198';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(198);
+  });
+
+  it('should not call onSetCountdown if not valid seconds entered', () => {
+    var spy = expect.createSpy();
+    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = 'asd';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
 });
